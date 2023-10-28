58036e9c82e8c5843481bc1176cc7d3080cff1b3
Pawan123Pawan <pawanmaurya4277@gmail.com>
2023-10-28 16:46:53 +0530

    change footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -14,16 +14,16 @@ const Footer = () => {
   return (
     <div className="footer">
       <div className="links">
-        <a href="https://github.com/ashwanimaurya85/Typing-Speed-Text" target="_blank">
+        <a href="https://github.com/Pawan123Pawan/" target="_blank">
           <GitHubIcon />
         </a>
-        <a href="https://www.linkedin.com/in/ashwani-kumar-48b28721a/" target="_blank">
+        <a href="https://www.linkedin.com/in/pawan-kumar-3515041a4/" target="_blank">
           <LinkedInIcon />
         </a>
-        <a href="https://twitter.com/Ashwani89142771" target="_blank">
+        <a href="https://twitter.com/Pawanma55680068" target="_blank">
           <TwitterIcon />
         </a>
-        <a href="https://www.instagram.com/ashwani.maurya.1213986/" target="_blank">
+        <a href="https://www.instagram.com/pawan_maurya.6/" target="_blank">
           <InstagramIcon />
         </a>
       </div>
